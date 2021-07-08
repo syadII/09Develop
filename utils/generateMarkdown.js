244ea5934377a25287035133c7264baf73090d4f
syadII <daysbiii@hotmail.com>
2021-07-08 00:26:26 -0700

    Complete?

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,23 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
+function renderLicenseSection(license) {
 
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
+function renderLicenseSection(license) {
 
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
-
-function renderMainPurpose(purpose) {
-  return
-}
 
 }
 
@@ -27,15 +22,28 @@ function renderMainTitle(title) {
   return `# ${title}`
 }
 
+function renderMainPurpose(purpose) {
+  return `# ${purpose}`
+}
+
+function renderMainDescription(description) {
+   return `# ${description}`
+}
+
 
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${renderMainTitle(data.title)}
-
+  
+${renderMainPurpose(data.purpose)}
 
 
 ${renderLicenseSection(data.title)}
+
+${renderLicenseSection(data.purpose)}
+
+${renderLicenseSection(data.description)}
 `;
 }
 
